a266700925c32c8ebdc03b7db21a3d7ef940b2f2
Chris Chambers <chrisgchambers@gmail.com>
2016-09-24 17:58:44 -0600

    Fix bug with nil value on first load

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 get '/' do
   plain_text = params["plain_text"]
   shift_by = params["shift_by"].to_i
-  code_output = caesar_cipher(plain_text, shift_by)
+  code_output = plain_text ? caesar_cipher(plain_text, shift_by) : ''
   erb :index, :locals => {:code_output => code_output,
                           :shift_by => shift_by,
                           :plain_text => plain_text
@@ -11,7 +11,7 @@ get '/' do
 end
 
 
-def caesar_cipher(unencoded, shift_by = 1)
+def caesar_cipher(unencoded='', shift_by = 1)
   encoded = unencoded.split('').collect do |character|
     # we're going to shift all letters and digits
     shift_by.times { character.next! } if character =~ /[a-zA-Z0-9]/
