87962b1c30afe37f271b412e8aabc1298c734e31
Chris Chambers <chrisgchambers@gmail.com>
2016-09-24 16:13:26 -0600

    html into view, add form for input

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/' do
-  'Hello World!'
+  erb :index
+end
+
+
+def caesar_cipher(unencoded, shift_by = 1)
+  encoded = unencoded.split('').collect do |character|
+    # we're going to shift all letters and digits
+    shift_by.times { character.next! } if character =~ /[a-zA-Z0-9]/
+      # .next adds an index letter if we go past z, Z, 9 - this removes it
+    character = character[-1]
+    character
+  end
+  encoded.join('')
 end
